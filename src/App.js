84a8766845cbbf64392fa049910934d4f13314d3
PrantoMollick <prantocse01@gmail.com>
2022-03-06 18:35:29 +0600

    directory menu and scss style add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import HomePage from './homepage-component';
+
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <HomePage />
     </div>
   );
 }
