897cd0809bde3934d45195507bc65a19b77fec9e
PrantoMollick <prantocse01@gmail.com>
2022-03-11 09:05:37 +0600

    update our menu items"

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
-
 import HomePage from './pages/homepage/homepage-component';
 
-
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+const Hats = () => (
+  <div>
+    <h1>Hats Component</h1>
+  </div>
+);
 
 function App() {
   return (
-    <React.Fragment>
-      <HomePage />
-    </React.Fragment>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/shop/hats' element={<Hats />} />
+      <Route
+      path="*"
+      element={
+        <main style={{ padding: "1rem" }}>
+          <p>There's nothing here!</p>
+        </main>
+      }
+    />
+    </Routes>
   );
 }
 
